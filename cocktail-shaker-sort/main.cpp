fca572bc72515f95521530a0e377bf461faebce2
Sergii Lovygin <sergey.lovygin@yahoo.com>
2022-10-24 20:53:03 +0300

    do base cleanup for `cocktail-shaker-sort`

--- cocktail-shaker-sort/main.cpp
+++ cocktail-shaker-sort/main.cpp
@@ -1,60 +1,78 @@
 #include <iostream>
 
-using namespace std;
 
-void output(int N, int** matrix)
-{
-    cout << "---------------------------------------------------------------\n";
+//
+// i/o
+//
+int read_int() {
+    int value = 0;
+    std::cin >> value;
+    return value;
+}
+
+void print_matrix(const int* const* matrix, std::size_t dimension) {
     int count = 0;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++)
-        {
-            if (matrix[i][j] < 10)
-                cout << " ";
-            cout << matrix[i][j] << "| ";
-            count += 1;
+    for (int i = 0; i < dimension; i++) {
+        for (int j = 0; j < dimension; j++) {
+            if (matrix[i][j] < 10) std::cout << " ";
+            std::cout << matrix[i][j] << "| ";
+            count++;
         }
-        if (count == N) { cout << "\n"; count = 0; }
+
+        if (count == dimension) {
+            std::cout << "\n";
+            count = 0;
+        }
     }
 }
-int** generator_matrix(int N, int** matrix)
-{
-    for (int i = 0; i < N; i++)
-        matrix[i] = new int[N];
 
-    for (int i = 0; i < N; i++)
-        for (int j = 0; j < N; j++)
-        {
+int** generator_matrix(std::size_t dimension) {
+    int** matrix = new int*[dimension];
+
+    for (int i = 0; i < dimension; i++) {
+        matrix[i] = new int[dimension];
+    }
+
+    for (int i = 0; i < dimension; i++) {
+        for (int j = 0; j < dimension; j++) {
             matrix[i][j] = rand() % 50;
         }
-    return matrix;
-}
-int** cocktail(int N, int** matrix)
-{
-    int tmp;
-    for (int p = 0; p < N; p++)
-    {
-        for (int j = 0; j < N-1; j++)
-            for (int i = 0; i < N-1; i++)
-            {
-                if (matrix[j][i] > matrix[j+1][i])
-                {
-                    tmp = matrix[j][i];
-                    matrix[j][i] = matrix[j+1][i];
-                    matrix[j+1][i] = tmp;
-                }
-            }
     }
+
     return matrix;
 }
 
-int main()
-{
-    int N;
-    cin >> N;
-    int** matrix = new int* [N];
-    generator_matrix(N, matrix);
-    output(N, matrix);
-    cocktail(N, matrix);
-    output(N, matrix);
+
+//
+// solution
+//
+int** sort_matrix_columns(int** matrix, std::size_t dimension) {
+    for (int p = 0; p < dimension; p++) {
+        for (int j = 0; j < dimension - 1; j++) {
+            for (int i = 0; i < dimension - 1; i++) {
+                if (matrix[j][i] > matrix[j + 1][i]) {
+                    int tmp          = matrix[j][i];
+                    matrix[j][i]     = matrix[j + 1][i];
+                    matrix[j + 1][i] = tmp;
+                }
+            }
+        }
+    }
+
+    return matrix;
+}
+
+
+//
+// application entry point
+//
+int main() {
+    const std::size_t dimension = read_int();
+    int** matrix = generator_matrix(dimension);
+    print_matrix(matrix, dimension);
+
+    sort_matrix_columns(matrix, dimension);
+    print_matrix(matrix, dimension);
+
+    return 0;
 }
