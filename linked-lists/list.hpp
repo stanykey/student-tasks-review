846bd7fae561abe01c0b49081003d43cf3e40a72
Sergii Lovygin <sergey.lovygin@yahoo.com>
2022-12-02 14:11:59 +0200

    [wip] save refactoring in list.hpp

--- linked-lists/list.hpp
+++ linked-lists/list.hpp
@@ -2,110 +2,110 @@
 #define LIST_HPP
 
 #include <iostream>
-struct Element {
-    int      data;
-    Element* next;
-    Element* prev;
-};
 
-void pop(Element*& start) {
-    if (start == nullptr) std::cout << "The list is empty" << std::endl;
-    else {
-        Element* p      = start->next;
-        int      result = p->data;
-        start->next     = p->next;
-        delete p;
-    }
-}
 
-void delete_list(Element*& list) {
-    if (list == nullptr) std::cout << "The list is empty" << std::endl;
-    else {
-        while (list != list->next) pop(list);
-        delete list;
-        list = nullptr;
-    }
-}
+namespace list {
+    struct node {
+        int   data{0};
+        node* next{nullptr};
+        node* prev{nullptr};
+    };
 
-void push(Element** start, int data) {
-    if (*start == nullptr) {
-        Element* new_node = new Element;
-        new_node->data    = data;
-        new_node->next = new_node->prev = new_node;
-        *start                          = new_node;
-        return;
+    void delete_list(node*& head) {
+        while (head) {
+            node* temp = head;
+            head       = head->next;
+            delete temp;
+        }
+        head = nullptr;
     }
 
-    Element* last     = (*start)->prev;
-    Element* new_node = new Element;
-    new_node->data    = data;
-    new_node->next    = *start;
-    (*start)->prev    = new_node;
-    new_node->prev    = last;
-    last->next        = new_node;
-}
+    void push(node** start, int data) {
+        if (*start == nullptr) {
+            node* new_node = new node;
+            new_node->data = data;
+            new_node->next = new_node->prev = new_node;
+            *start                          = new_node;
+            return;
+        }
 
-void output(struct Element* start) {
-    Element* temp = start;
-    Element* last = start->prev;
-    temp          = last;
-    std::cout << "List: ";
-    while (temp->prev != last) {
-        std::cout << temp->data << " <-> ";
-        temp = temp->prev;
+        node* last     = (*start)->prev;
+        node* new_node = new node;
+        new_node->data = data;
+        new_node->next = *start;
+        (*start)->prev = new_node;
+        new_node->prev = last;
+        last->next     = new_node;
     }
-    std::cout << temp->data << std::endl;
-}
 
-void swap(struct Element* start, int current, int amount) {
-    Element* tmp = start;
-    for (int i = 0; i < amount; i++) {
-        tmp = tmp->prev;
-
-        if (tmp->data == current) {
-            int left, right;
-            tmp       = tmp->prev;
-            left      = tmp->data;
-            tmp       = tmp->next;
-            tmp       = tmp->next;
-            right     = tmp->data;
-            tmp->data = left;
-            tmp       = tmp->prev;
-            tmp       = tmp->prev;
-            tmp->data = right;
+    void pop(node*& start) {
+        if (start == nullptr) std::cout << "The list is empty" << std::endl;
+        else {
+            node* p      = start->next;
+            int   result = p->data;
+            start->next  = p->next;
+            delete p;
         }
     }
-}
 
-void input(struct Element** start, int amount) {
-    for (int i = 0; i < amount; i++) {
-        int element;
-        std::cout << "Enter new element: ";
-        std::cin >> element;
-        push(start, element);
+    void output(struct node* start) {
+        node* temp = start;
+        node* last = start->prev;
+        temp       = last;
+        std::cout << "List: ";
+        while (temp->prev != last) {
+            std::cout << temp->data << " <-> ";
+            temp = temp->prev;
+        }
+        std::cout << temp->data << std::endl;
     }
-}
 
-void user() {
-    int      amount, current;
-    Element* start = nullptr;
+    void swap(struct node* start, int current, int amount) {
+        node* tmp = start;
+        for (int i = 0; i < amount; i++) {
+            tmp = tmp->prev;
 
-    std::cout << "Enter amount elements: ";
-    std::cin >> amount;
+            if (tmp->data == current) {
+                int left, right;
+                tmp       = tmp->prev;
+                left      = tmp->data;
+                tmp       = tmp->next;
+                tmp       = tmp->next;
+                right     = tmp->data;
+                tmp->data = left;
+                tmp       = tmp->prev;
+                tmp       = tmp->prev;
+                tmp->data = right;
+            }
+        }
+    }
 
-    input(&start, amount);
-    output(start);
+    void input(struct node** start, int amount) {
+        for (int i = 0; i < amount; i++) {
+            int element;
+            std::cout << "Enter new node: ";
+            std::cin >> element;
+            push(start, element);
+        }
+    }
 
-    std::cout << "Enter current: ";
-    std::cin >> current;
+    void demo() {
+        int   amount, current;
+        node* start = nullptr;
 
-    swap(start, current, amount);
-    output(start);
-    delete_list(start);
-}
+        std::cout << "Enter amount elements: ";
+        std::cin >> amount;
 
-int main() {
-    user();
-}
+        input(&start, amount);
+        output(start);
+
+        std::cout << "Enter current: ";
+        std::cin >> current;
+
+        swap(start, current, amount);
+        output(start);
+        delete_list(start);
+    }
+}  // namespace list
 
 #endif  // LIST_HPP
